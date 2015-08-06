501218bd494e59224f5db3b1b0abedf291f6dc9c
Jetroid <holtjet@gmail.com>
2015-08-06 11:03:09 +0100

    Changed this module to configure pam_mount.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,14 +1,18 @@
 class mounthomedir::install (
-  $ensure                   = $mounthomedir::ensure,
-  $homedir_packages         = $mounthomedir::homedir_packages,
-  $ldap_base_dn             = $mounthomedir::ldap_base_dn,
-  $ldap_uri                 = $mounthomedir::ldap_uri,
-  $fallback_homedirs_server = $mounthomedir::fallback_homedirs_server,
+  $ensure                       = $mounthomedir::ensure,
+  $scripts_ensure               = $mounthomedir::scripts_ensure,
+  $pam_mount_config             = $mounthomedir::pam_mount_config
+  $homedir_packages             = $mounthomedir::homedir_packages,
+  $ldap_base_dn                 = $mounthomedir::ldap_base_dn,
+  $ldap_uri                     = $mounthomedir::ldap_uri,
+  $fallback_homedirs_server     = $mounthomedir::fallback_homedirs_server,
+  $default_homedirs_server_fqdn = $mounthomedir::default_homedirs_server_fqdn
+  $custom_mount_options         = $mounthomedir::custom_mount_options
 ) {
 
-  # Defaults for the file resource type.
+  # Defaults for the two scripts.
   File {
-    ensure  => present,
+    ensure  => $scripts_ensure,
     owner   => root,
     group   => root,
     mode    => "755",
@@ -16,16 +20,21 @@ class mounthomedir::install (
 
   # Automounted home directories
   package{$homedir_packages:
-    ensure => present,
+    ensure => $ensure,
+  }
+
+  class { 'pam_mount':
+    ensure => $ensure
+    config => $pam_mount_config
   }
 
   file{ '/usr/local/bin/doautomount':
     content => template('mounthomedir/doautomount.erb'),
-    require Class['pam_mount'],
+    require => Class[pam_mount]
   }
 
   file{ '/usr/local/bin/doautounmount':
     source  => "puppet:///modules/mounthomedir/doautounmount",
-    require Class['pam_mount'],
+    require => Class[pam_mount]
   }
 }
