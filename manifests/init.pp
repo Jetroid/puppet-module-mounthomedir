e4aa75c26658274dd2b00171d4269aadeb518fc3
Jetroid <holtjet@gmail.com>
2015-08-06 11:03:48 +0100

    Added more parameters.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,16 +3,22 @@
 # Mount the user's home directory. 
 #
 class mounthomedir (
-  $ensure                   = $mounthomedir::params::ensure,
-  $homedir_packages         = $mounthomedir::params::homedir_packages,
-  $ldap_base_dn             = $mounthomedir::params::ldap_base_dn,
-  $ldap_uri                 = $mounthomedir::params::ldap_uri,
-  $fallback_homedirs_server = $mounthomedir::params::fallback_homedirs_server,
+  $ensure                       = $mounthomedir::params::ensure,
+  $scripts_ensure               = $mounthomedir::params::scripts_ensure,
+  $pam_mount_config             = $mounthomedir::params::pam_mount_config
+  $homedir_packages             = $mounthomedir::params::homedir_packages,
+  $ldap_base_dn                 = $mounthomedir::params::ldap_base_dn,
+  $ldap_uri                     = $mounthomedir::params::ldap_uri,
+  $fallback_homedirs_server     = $mounthomedir::params::fallback_homedirs_server,
+  $default_homedirs_server_fqdn = $mounthomedir::params::default_homedirs_server_fqdn
+  $custom_mount_options         = $mounthomedir::params::custom_mount_options
 ) inherits mounthomedir::params {
 
   validate_re($ensure, '^(present|absent)$',"${ensure} is not allowed for the 'ensure' parameter. Allowed values are 'present' and 'absent'.")
 
-  validate_array($homedir_packages,)
+  validate_array($homedir_packages,$custom_mount_options, $pam_mount_config, $ldap_uri)
+
+  validate_string($default_homedirs_server_fqdn,$fallback_homedirs_server, $ldap_base_dn)
 
   anchor { 'mounthomedir::begin': } ->
   class { '::mounthomedir::install': } ->
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,6 +2,26 @@
 class mounthomedir::params {
 
   $ensure = 'present'
+  $scripts_ensure = $ensure
+  $pam_mount_config = [
+      ['debug', {'enable' =>  '"0"'}],
+      [path, '"/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"'],
+      # Custom mount/unmount scripts; these automatically find the user's 
+      # home filestore location etc.
+      ['cifsmount','doautomount %(USER) %(MNTPT) %(USERUID) %(USERGID)'],
+      ['umount','"doautounmount %(MNTPT)"'],
+      # Always cifs mount home directories; use server in $default_homedirs_server_fqdn (eg: foo.bar.example.co.uk) by default.
+      ['volume', {
+        'fstype'     => '"cifs"',
+        'server'     => "\"${default_homedirs_server_fqdn}\"",
+        'path'       => '"%(USER)"',
+        'mountpoint' => '"~"',}
+      ]
+      # Note for Future People from Space: do not set sgrp
+      # to Domain Users in the volume tag. It'll get cached
+      # with only the previous people it's seen in that group,
+      # and people won't be able to mount homedirs.
+    ]
   
   # Packages needed for samba-mounted home directories.
   case $osfamily {
@@ -13,7 +33,7 @@ class mounthomedir::params {
         'keyutils',
         'libsasl2-modules-gssapi-mit',
         'ldap-utils',
-        #'krb5-user',
+        'krb5-user',
       ]
     }
     /RedHat/: {
@@ -24,7 +44,7 @@ class mounthomedir::params {
         'keyutils',
         #'libsasl2-modules-gssapi-mit',
         'openldap-clients',
-        #'krb5-workstation',
+        'krb5-workstation',
       ]
     }
     /Default/: {
@@ -36,7 +56,12 @@ class mounthomedir::params {
 
   $ldap_uri = ['dc=bar','dc=example','dc=co','dc=uk']
 
-  #If homedirs server is foo.bar.example.co.uk, use foo. 
-  $fallback_homedirs_server = 'foo' 
+  #If homedirs server is foo.bar.example.co.uk, use foo.
+  #Server to use if all else fails.
+  $fallback_homedirs_server = 'foo'
+
+  $default_homedirs_server_fqdn = 'foo.bar.example.co.uk'
+
+  $custom_mount_options = ['nobrl','serverino','_netdev']
 
 }
