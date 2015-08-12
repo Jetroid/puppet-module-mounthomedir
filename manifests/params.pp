80b4362ea08431706a10fe059fd1b9ff4453549a
Jetroid <holtjet@gmail.com>
2015-08-12 16:44:07 +0100

    Proposal for non-script based mounting.

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,20 +4,36 @@ class mounthomedir::params {
   $ensure = hiera('mounthomedir::ensure', 'present')
   $scripts_ensure = $ensure
   $default_homedirs_server_fqdn = hiera('mounthomedir::default_homedirs_server_fqdn', 'foo.bar.example.co.uk')
+  #If homedirs server is foo.bar.example.co.uk, use foo.
+  #Server to use if all else fails.
+  $fallback_homedirs_server = hiera('mounthomedir::fallback_homedirs_server', 'foo')
+
+  $custom_mount_options = hiera('mounthomedir::custom_mount_options', ['nobrl','serverino','_netdev'])
+  $opts = join($custom_mount_options, ",")
+
+  $security_type = hiera('mounthomedir::security_type', ['krb5','cruid=%(USERUID)'])
+  $sectype = join($security_type, ",")
+
   $pam_mount_config = [
-      ['debug', {'enable' =>  '1'}],
+      ['debug', {'enable' =>  '0'}],
       [path, '/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin'],
       # Custom mount/unmount scripts; these automatically find the user's 
       # home filestore location etc.
       ['cifsmount','doautomount %(USER) /home/%(USER) %(USERUID) %(USERGID)'],
+      #Alternate:
+      #['cifsmount',"mount -t cifs //${fallback_homedirs_server}/%(USER) /home/%(USER) -o ${opts},sec=${sectype}"],
       ['umount','doautounmount /home/%(USER)'],
+      #Alternate:
+      #['umount','umount /home/%(USER)']
       # Always cifs mount home directories; use server in $default_homedirs_server_fqdn (eg: foo.bar.example.co.uk) by default.
       ['volume', {
         'fstype'     => 'cifs',
         'server'     => "${default_homedirs_server_fqdn}",
         'path'       => '%(USER)',
         'mountpoint' => '',}
-      ]
+      ],
+      ['msg-authpw','Please enter your password:'],
+      ['msg-sessionpw','Please enter your password:'],
       # Note for Future People from Space: do not set sgrp
       # to Domain Users in the volume tag. It'll get cached
       # with only the previous people it's seen in that group,
@@ -52,15 +68,5 @@ class mounthomedir::params {
       fail('mounthomedir only supports Debian and RedHat OS families.')
     }
   }
-
-  $ldap_base_dn = 'DC=bar,DC=example,DC=co,DC=uk'
-
-  $ldap_uri = ['ldap://baz0.bar.example.co.uk','ldap://baz1.bar.example.co.uk','ldap://baz2.bar.example.co.uk']
-
-  #If homedirs server is foo.bar.example.co.uk, use foo.
-  #Server to use if all else fails.
-  $fallback_homedirs_server = 'foo'
-
-  $custom_mount_options = ['nobrl','serverino','_netdev']
 
 }
